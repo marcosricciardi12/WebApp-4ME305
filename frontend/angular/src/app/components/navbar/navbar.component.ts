c93bbdd2c6097f5db81f513085f81799230ccafa
marcosricciardi12 <marcosricciardi12@gmail.com>
2023-02-01 05:48:03 +0100

    front angular

---
+++ frontend/angular/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  token: any;
+  admin = true;
+  user:any = "";
+  constructor(
+    private authService:AuthService
+  ) { }
+
+  ngOnInit(): void {
+    let token = localStorage.getItem("token") || "";
+    if (token) {
+      let decodedJWT = JSON.parse(window.atob(token.split('.')[1]));
+      this.user = decodedJWT.user
+    }
+    
+  }
+
+  get isToken() {
+    return localStorage.getItem('token') || undefined;
+ }
+
+ 
+ cerrarSesion() {
+  this.authService.logout();
+}
+
+}
---
+++ frontend/angular/src/app/services/auth/auth.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable, take } from 'rxjs';
+import Swal from 'sweetalert2';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Access-Control-Allow-Methods': 'POST'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  
+  url = 'auth/login'
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router
+  ) { }
+  
+  login(dataLogin:any): Observable<any> {
+    //dataLogin = {email: 'email@gmail.com',password: '123456'};
+    return this.httpClient
+      .post(this.url , dataLogin, httpOptions)
+      .pipe(take(1));
+  }
+
+
+  logout() {
+    
+    localStorage.removeItem('token');
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Logged out!',
+      showConfirmButton: false,
+      timer: 1500
+    })
+    this.router.navigate(['/', 'home']);
+    
+  }
+
+}
