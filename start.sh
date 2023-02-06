6ac595655ececda656080b52ad1bbf33e70b7723
marcosricciardi12 <marcosricciardi12@gmail.com>
2023-02-06 16:22:55 -0300

    added start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+cd backend
+./install.sh
+./boot.sh
+cd ..
+cd frontend/angular
+npm install
+ng build --configuration=production
+docker run -d -p 8080:80 -v $PWD/dist/client:/usr/share/nginx/html nginx:alpine
